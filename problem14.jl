978fc1753cfdf50df4b86878d9d09039569411c3
yqwu <pingline@outlook.com>
2021-06-27 01:17:20 +0800

    修复序号错误

--- problem14.jl
+++ problem14.jl
@@ -1,26 +1,16 @@
-using ProgressMeter
+dim = 21
 
-function chainLength(a::Int64)::Int64
-    f = a
-    len = 1
-    while(f!=1)
-        # println(f)
-        f = isodd(f) ? 3*f + 1 : div(f,2);
-        len+=1;
-    end
-    return len
+a = ones(Int64, dim, dim)
+
+for i in 1:dim
+    a[i, 1] = 1
+    a[1, i] = 1
 end
 
-# println(chainLength(13));
-max = 0
-p = 0
-@showprogress for i in 1:1000000
-    global max, p
-    l = chainLength(i)
-    if l > max
-        max = l
-        p = i
+for i in 2:dim
+    for j in 2:dim
+        a[i, j] = a[i-1, j] + a[i, j-1]
     end
 end
 
-println(p)
+println(a[dim, dim])
--- problem15.jl
+++ problem15.jl
@@ -1,16 +1,13 @@
-dim = 21
+using Printf
 
-a = ones(Int64, dim, dim)
+a = BigInt(2)^1000
 
-for i in 1:dim
-    a[i, 1] = 1
-    a[1, i] = 1
+str = @sprintf "%d" a
+
+sum = 0
+for i in str
+    global sum
+    sum += parse(Int64, i)
 end
 
-for i in 2:dim
-    for j in 2:dim
-        a[i, j] = a[i-1, j] + a[i, j-1]
-    end
-end
-
-println(a[dim, dim])
+println(sum)
